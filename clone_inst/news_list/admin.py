8bf15ff787c632a1956a70c1f7cf6e7ace8240b6
asg1n <yegor.krivoruchko.2009@mail.ru>
2023-10-15 13:29:58 +0700

    fcommit

--- clone_inst/news_list/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Post, Likes, Subscribes
-
-admin.site.register(Post)
-admin.site.register(Likes)
-admin.site.register(Subscribes)
--- clone_inst/news_list/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import NewsList, AddPhoto
- 
-urlpatterns = [
-    path('news/', NewsList.as_view(), name='news'),
-    path('add_photo/', AddPhoto.as_view(), name='add_photo'),
-]
